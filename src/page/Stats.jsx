6983627b72b60189794f1d137f628b4cce1528da
ovaphlow <ovaphlow@live.com>
2022-06-01 15:18:12 +0800

    recharts

--- src/page/Stats.jsx
+++ src/page/Stats.jsx
@@ -1,8 +1,15 @@
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
 import Box from "../component/Box";
 import Layout from "../component/Layout";
 import PageTitle from "../component/PageTitle";
 
 export default function Stats() {
+    const data = [
+        { name: "page a", uv: 400, pv: 2400, amt: 2400 },
+        { name: "page b", uv: 100, pv: 2400, amt: 2400 },
+        { name: "page c", uv: 700, pv: 2400, amt: 2400 },
+    ];
+
     return (
         <Layout>
             <PageTitle text="统计数据" />
@@ -10,6 +17,17 @@ export default function Stats() {
             <Box>
                 <div className="grid gap-4">
                     <p className="text-xl">统计图表</p>
+                    <div className="w-full h-64">
+                        <ResponsiveContainer>
+                            <LineChart width={600} height={400} data={data}>
+                                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                                <CartesianGrid stroke="#CCC" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
                 </div>
             </Box>
         </Layout>
